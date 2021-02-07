4334d79e443f4eab379d5b456a7a0033c8a8c49c
JonasCOliveira <jonascauan@outlook.com>
2021-02-07 13:39:42 -0300

    v01

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Animator anim;
+    public float runSpeed = 10f;
+    public bool isRight;
+
+    void Update() {
+
+        if(isRight){
+
+            transform.Translate( - 1 *runSpeed * Time.deltaTime, 0,0);
+            transform.localScale = new Vector2(2,2);
+    
+        } else{
+
+            transform.Translate(  1 *  runSpeed * Time.deltaTime, 0,0);
+            transform.localScale = new Vector2(-2,2);
+    
+        }
+
+    }  
+
+    void OnTriggerEnter2D(Collider2D trig){
+
+
+        if(trig.gameObject.CompareTag("turnLeftRight")){
+
+            if(isRight){
+                isRight = false;
+            }
+            else {
+                isRight = true;
+            }
+
+        }
+
+        if(trig.gameObject.CompareTag("Bullet")){
+
+            StartCoroutine(DestroyOpossum());
+        }
+
+    }
+
+    IEnumerator DestroyOpossum(){
+
+        anim.SetBool("isDead", true);
+        yield return new WaitForSeconds(.25f);
+        Destroy(gameObject);
+        
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# JourneyRooz
- 
+# PCG-Game
